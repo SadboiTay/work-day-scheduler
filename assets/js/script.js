440aee8179475dea6702db21bdba0f114ce52830
SadboiTay <tg.hakes@gmail.com>
2021-08-20 16:49:33 -0600

    create time-blocks, begin script- click event for blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,35 @@
 var currentTime = moment().format("dddd, MMM Do");
-console.log(currentTime);
 
 // display current date/time in header
 $("#currentDay").text(currentTime);
+
+// schedule area was clicked
+// click event on .col-10?
+$("#block").on("click", function() {
+    // opens input for text
+    var textInput = $("<textarea>")
+        .addClass("textarea")
+        
+    $("#block").append(textInput);
+    
+    
+    // focus on input
+    textInput.trigger("focus");
+    // create function for save button click
+})
+
+// save button was clicked
+
+
+// save function
+    // get text value from #text
+
+    // push to array
+
+    // save to localStorage
+
+
+// load function
+    //  get from localStorage
+
+
