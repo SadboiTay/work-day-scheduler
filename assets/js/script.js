338f213b1c176a57ed03ef9ecd838848f850816b
SadboiTay <tg.hakes@gmail.com>
2021-08-20 17:23:37 -0600

    textarea is blurred event

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,28 +6,51 @@ $("#currentDay").text(currentTime);
 // schedule area was clicked
 // click event on .col-10?
 $("#block").on("click", function() {
-    // opens input for text
-    var textInput = $("<textarea>")
-        .addClass("textarea")
-        
-    $("#block").append(textInput);
+    // grabs current text, if any
+    var text = $("#text").text().trim();
     
+    // create textarea
+    var textInput = $("<textarea>")
+    .addClass("textarea")
+    .val(text);
+    
+    // swap <p> with texarea
+    $("#text").replaceWith(textInput);
     
     // focus on input
     textInput.trigger("focus");
-    // create function for save button click
+})
+
+// textarea is blurred
+$("#block").on("blur", "textarea", function() {
+    console.log("blur")
+    // grab value from textarea
+    var text = $(this).val().trim();
+
+    // create <p>
+    var textP = $("<p>").addClass("p-3").attr("id", "text").text(text);
+
+    // swap textarea with <p>
+    $(this).replaceWith(textP);
 })
 
 // save button was clicked
-
+$("button").on("click", function() {
+    saveText();
+})
 
 // save function
-    // get text value from #text
-
+var saveText = function() {
+    // get text value from textarea
+    var text = $("textarea").text().trim();
+    console.log(text);
+    
     // push to array
-
+    
     // save to localStorage
-
+    
+}
+    
 
 // load function
     //  get from localStorage
